6f4092314a73de7e90183374acc799ddb373acdc
bibildibabeldibub <nico_schaefer@web.de>
2025-01-14 19:59:39 +0100

    Task 2a Init theme
    - init theme for task 2a

---
+++ shopware/custom/plugins/NSCTaskTheme/src/NSCTaskTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace NSCTaskTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class NSCTaskTheme extends Plugin implements ThemeInterface
+{
+}
